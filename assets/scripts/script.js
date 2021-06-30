77d6b5d6c41804d1fdaa79c0db38d1f82262ce64
JessGiannini <jessgiannini@gmail.com>
2021-06-30 11:44:52 -0700

    starting the js

---
+++ assets/scripts/script.js
@@ -0,0 +1,9 @@
+var dateTimeEl = document.querySelector(".dateTime");
+
+dateTimeEl = moment().format("MMM Do, YYYY" "hh:mm:ss")
+
+function setTime(){
+    var timerCount = setInterval(function(){
+
+    }, 1000)
+}
